059ed36d4c78111f573ffd1066ec0454907ba021
Linkun Wan <lw2861@nyu.edu>
2024-03-27 13:45:20 -0400

    upload readme bug fixed

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='mksv_miniGame',
-    version='0.1.6',
+    version='0.1.7',
     author='Linkun Wan, Vanessa Viglucci, Sara Gizabi, Marcus Du',
     author_email='lw2861@nyu.edu, vcv227@nyu.edu, sg7029@nyu.edu, mrd9556@nyu.edu',
     description='Some mini-games using Pygame',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     package_data={
         'minigame': ['*.png'], 
     },
