0bfb42dc6404a709057c2956b44430f12b15ec28
Quadram13 <marcus.du@gmail.com>
2024-03-26 20:18:47 -0400

    update setup
    added name and email

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='miniGame',
     version='0.1.0',
-    author='Linkun Wan, Vanessa Viglucci, Sara Gizabi',
-    author_email='lw2861@nyu.edu, vcv227@nyu.edu, sg7029@nyu.edu',
+    author='Linkun Wan, Vanessa Viglucci, Sara Gizabi, Marcus Du',
+    author_email='lw2861@nyu.edu, vcv227@nyu.edu, sg7029@nyu.edu, mrd9556@nyu.edu',
     description='Some mini-games using Pygame',
     long_description=open('README.md').read(),
     install_requires=[
