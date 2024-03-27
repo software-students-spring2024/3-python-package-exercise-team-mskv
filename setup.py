0917f1a8507d139f7fac6a5c0a915b3338cdf836
Linkun Wan <lw2861@nyu.edu>
2024-03-27 02:04:06 -0400

    bug fixed for image problem

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup
 
 setup(
     name='mksv_miniGame',
-    version='0.1.2',
+    version='0.1.6',
     author='Linkun Wan, Vanessa Viglucci, Sara Gizabi, Marcus Du',
     author_email='lw2861@nyu.edu, vcv227@nyu.edu, sg7029@nyu.edu, mrd9556@nyu.edu',
     description='Some mini-games using Pygame',
     long_description=open('README.md').read(),
+    package_data={
+        'minigame': ['*.png'], 
+    },
     install_requires=[
         "pygame>=2.5.2",
     ],
